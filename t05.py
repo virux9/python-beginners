16cbf1e57db14c482c6f106ed284e1f67d447b68
virux9 <virux9@gmail.com>
2020-06-07 16:18:43 +0500

    теперь добавляет соответственно условиям задачи

--- t05.py
+++ t05.py
@@ -2,7 +2,7 @@
 my_list = [7, 5, 3, 3, 2]
 in_val = int(input("Enter value:"))
 if my_list.count(in_val) > 0:
-    my_list.insert(my_list.index(in_val),in_val)
+    my_list.insert(my_list.index(in_val) + my_list.count(in_val), in_val)
 else:
-    my_list.insert(0,in_val)
+    my_list.insert(0, in_val)
 print(my_list)
